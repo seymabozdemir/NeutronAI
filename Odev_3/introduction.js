98f24a7399f1c839c903e17a01696239a2783041
seymabozdemir <seymabozdeemir@gmail.com>
2026-03-15 14:59:42 +0300

    3.Hafta Odevi

---
+++ Odev_3/introduction.js
@@ -0,0 +1,29 @@
+//console.log('bu yazı introduction.js den geldi!')
+//console.log(typeof 'Prototurk')
+//console.log(typeof 5 )
+//console.log(typeof true )
+//console.log(typeof null )
+//console.log(typeof undefined )
+
+/*değişken tanımlamanın iki yolu var.
+1)let: aynı script içerisinde ona yeniden değer atayabiliriz.
+2)const:Değerinden sabit olduğumuz bir değeri const ile oluşturuyoruz.
+*/
+/*Javascrip değişken adı tanımlarken sayı ile başlayamazsın 
+$  ve _ dışında değişken siminde özel karakter kullanılamaz.*/
+
+//let name /*Böyle değişken tanımlanabilir. */
+//let $num1 /*Böyle değişken tanımlanabilir. */
+/*let ile değişken tanımlarken ya her değişkenin başına let yazabiliriz ya da aşağıdaki gibi yapabiliriz. */
+
+let youtubeChannel = 'prototurk',
+     channelAge = 11,
+     isChannelStillActive = true,
+     secondChannel = null,
+     channelMoney = undefined
+
+const PI = 3.14
+const GRAVITY = 9.81
+
+console.log(youtubeChannel, channelAge, isChannelStillActive, secondChannel, channelMoney )
+ 
